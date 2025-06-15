dc4cba4e24da2d322c11b019da0cdc3e7517dcdd
THISHUKUMAR <thishukumarab77@gmail.com>
2025-06-15 20:29:27 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name='Generative AI Project',
-    version='0.0.0',
-    author='Thishukumar maharaj',
-    author_email='thishukumarab77@gmail.com',
-    packages=find_packages(),
-    install_requires=[]
-)
