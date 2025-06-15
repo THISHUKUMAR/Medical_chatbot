c0ddbfdb1a1336e93430bf0dfbd57d252c311f5c
THISHUKUMAR <thishukumarab77@gmail.com>
2025-06-15 19:55:52 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Thishukumar maharaj',
+    author_email='thishukumarab77@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
